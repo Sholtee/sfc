bd30cfd7decc89bac51131890a90e0dffb69ee25
Solti Dénes <denes.solti@oh.gov.hu>
2018-08-24 09:56:14 +0200

    sanitizeInput()

--- tasks/sfc.js
+++ tasks/sfc.js
@@ -24,7 +24,7 @@ module.exports = grunt => grunt.registerMultiTask('sfc', 'Single File Component'
         // (az utana levoket pedig figyelmen kivul hagyja az ertelmezo).
         //
 
-        const wrap = '<data>' + fs.readFileSync(filePath).toString().replace(/&/g, '&amp;') + '</data>';
+        const wrap = '<data>' + sanitizeInput(fs.readFileSync(filePath).toString()) + '</data>';
         parse(wrap, {async: false, normalizeTags: true, explicitRoot: false, attrkey: '$$attrs', charkey: '$$text'}, (err, xml) => {
             if (err) throw err;
             var processed = 0;
@@ -87,5 +87,38 @@ module.exports = grunt => grunt.registerMultiTask('sfc', 'Single File Component'
 
             grunt.log.writeln(processed + ' file(s) created');
         });
+
+        function sanitizeInput(input){
+            var sanitized = '';
+
+            //
+            // Egy melysegben match-elunk a node-okra es azok tartalmara (megjegyzem
+            // h ez a regex a commenteket [<!-- -->] kihagyja).
+            //
+
+            for(var ar, regex = /<(\w+)\b.*>([\s\S]*)<\/\1>/g; (ar = regex.exec(input)) != null;){
+                const
+                    match   = ar[0],
+                    content = ar[2];
+
+                //
+                // Tartalomban escape-elunk.
+                //
+
+                sanitized += match.replace(content, escape(content, {
+                    '"': '&quot;',
+                    "'": '&apos;',
+                    '<': '&lt;',
+                    '>': '&gt;',
+                    '&': '&amp;'
+                }));
+            }
+
+            function escape(str, chars){
+                return str.replace(new RegExp('(' + Object.keys(chars).join('|') + ')', 'g'), (match, char) => chars[char]);
+            }
+
+            return sanitized;
+        }
     }));
 });
